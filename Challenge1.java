5ef29d4750b4dcecf297790471962ddc528b9257
vidya1551 <72929526+vidya1551@users.noreply.github.com>
2020-10-15 21:48:17 +0530

    Create Challenge1.java
    check given point inside or outside polygon.

---
+++ Challenge1.java
@@ -0,0 +1,28 @@
+
+
+import java.io.*;
+import java.util.*;
+class Challenge 
+{
+   public class Point
+	{
+	  int x;
+	  int y;
+	  
+	  public Point(int x,int y)
+	  {
+		  this.x=x;
+		  this.y=y;
+	  }
+	};
+  public static void main(String args[])
+  {
+	  Point polygon[]={ new Point(1,0),
+			            new Point(8,3),
+			            new Point(8,8),
+			            new Point(1,5)
+	  };
+	  
+	  Point p=new Point(3,5);
+  }
+}
